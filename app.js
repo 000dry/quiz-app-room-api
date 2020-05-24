38c94a56ce506ba025495a2c35d864ac29b96308
Joe Ryan <joe@Joes-iMac.local>
2020-05-24 16:55:33 +0100

    handle POST requests

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+// const roomServer = require('./libs/room-server');
 
-app.get('/', function (req, res) {
-    res.send("Yo quiz app")
-})
+app.post('/', function (req, res) {
+    res.send(req.body)
+  })
 
-app.listen(3000, function () {
-    console.log('Room app listening - get the party started on port 3000!');
-  });
+app.listen(4040, function () {
+    console.log('Room app listening - get the party started on port 4040!');
+});
+
+// roomServer.listen(server);
